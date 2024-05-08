de24abeedde029fdbae0e487fb904404ff0dad11
Caleb Dalgleish <Dalgleishc@uchicago.edu>
2024-05-08 17:12:10 -0500

    58

--- src/scripts/movement.py
+++ src/scripts/movement.py
@@ -1,158 +1,20 @@
 #!/usr/bin/env python3
-# import rospy
-# from geometry_msgs.msg import Twist
-# import moveit_commander
-# import math
-# from sensor_msgs.msg import Image, LaserScan
-
-# class Movement:
-#     '''
-#     Class for the movement of the robot
-#     '''
-#     def __init__(self):
-#         rospy.init_node('movement_controller')
-#         rospy.loginfo("Initializing Movement Node")
-#         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-#         # self.trash_detect = rospy.Subscriber('camera/rgb/image_raw', Image, self.detect_trash)
-#         self.scan_subscriber = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
-#         self.twist = Twist()
-#         self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
-#         self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
-#         rospy.wait(1)
-#         self.current_scan = None
-#         self.initialize_robot()
-
-#     def initialize_robot(self):
-#         rospy.loginfo("Initializing robot arm and gripper to default states...")
-#         self.move_group_arm.go([0, 0, 0, 0], wait=True)
-#         self.move_group_gripper.go([0.01, 0.01], wait=True)
-#         self.move_group_arm.stop()
-#         self.move_group_gripper.stop()
-#         rospy.loginfo("Robot initialized to home position with gripper open.")
-
-#     def pick_up_object(self):
-#         rospy.loginfo("Picking up the object...")
-#         self.move_group_arm.go([0, math.radians(18), math.radians(2), math.radians(0)], wait=True)
-#         rospy.sleep(2)
-#         self.move_group_gripper.go([-0.01, -0.01], wait=True)
-#         rospy.sleep(2)
-#         self.move_group_arm.go([0, -0.5, 0, 0], wait=True)
-#         rospy.sleep(2)
-#         self.move_group_arm.stop()
-#         self.move_group_gripper.stop()
-#         rospy.loginfo("Object picked up successfully.")
-
-#     def spin_around(self):
-#         rospy.loginfo("Spinning the robot...")
-#         self.twist.angular.z = 0.5  # Spin speed
-#         self.cmd_vel_pub.publish(self.twist)
-#         rospy.sleep(5)  # Spin for 2 seconds
-#         self.twist.angular.z = 0  # Stop the spin
-#         self.cmd_vel_pub.publish(self.twist)
-#         rospy.loginfo("Finished spinning.")
-
-#     def move_towards_object(self, distance):
-#         rospy.loginfo(f"Moving towards the object by {distance} meters...")
-#         self.twist.linear.x = 0.2  # Set forward speed
-#         self.cmd_vel_pub.publish(self.twist)
-#         rospy.sleep(distance / 0.2)
-#         self.twist.linear.x = 0  # Stop moving forward
-#         self.cmd_vel_pub.publish(self.twist)
-#         rospy.loginfo("Reached the object.")
-
-#     def throw_away(self):
-#         rospy.loginfo("Dumping the object into the bin at the back...")
-        
-#         # Adjust the arm to a position behind the robot, suitable for dumping into the bin
-#         dump_position = [0, -math.radians(30), -math.radians(10), math.radians(0)]  # Example: adjust angles as necessary
-#         self.move_group_arm.go(dump_position, wait=True)
-#         rospy.sleep(5)
-        
-#         # Open the gripper to release the object into the bin
-#         self.move_group_gripper.go([0.01, 0.01], wait=True)
-#         rospy.sleep(5)
-        
-#         # Return the arm to a default position
-#         self.move_group_arm.go([0, 0, 0, 0], wait=True)
-#         self.move_group_gripper.go([0.01, 0.01], wait=True)
-#         self.move_group_arm.stop()
-#         self.move_group_gripper.stop()
-        
-#         rospy.loginfo("Object dumped successfully into the bin.")
-
-#     # def detect_trash(self):
-#     #     '''
-#     #     This is a placeholder for the actual trash detection logic
-#     #     '''
-#     #     rospy.loginfo("Detecting trash...")
-#     #     pass
-
-#     def scan_callback(self, data):
-#         '''
-#         This callback processes LIDAR data to find the closest object
-#         '''
-#         self.current_scan = data
-
-#     def find_closest_object(self):
-#         if self.current_scan is None:
-#             return None
-#         ranges = self.current_scan.ranges
-#         min_distance = float('inf') or 0
-#         min_angle = None
-#         for i, distance in enumerate(ranges):
-#             if distance < min_distance:
-#                 min_distance = distance
-#                 min_angle = i
-#         return min_distance, min_angle
-
-#     def run(self):
-#         '''
-#         Main logic:
-#         - Spin around to scan the environment
-#         - Move towards the closest object
-#         - Pick up the object
-#         - Throw away the object
-#         '''
-#         rate = rospy.Rate(10)  # 10 Hz
-#         while not rospy.is_shutdown():
-#             # Spin around to find objects
-#             self.spin_around()
-#             closest_object = self.find_closest_object()
-#             if closest_object is not None:
-#                 min_distance, min_angle = closest_object
-#                 rospy.loginfo(f"Closest object at distance: {min_distance}, angle: {min_angle}")
-#                 self.move_towards_object(min_distance)
-#                 self.pick_up_object()
-#                 self.throw_away()
-#             else:
-#                 rospy.loginfo("No objects detected.")
-#             rate.sleep()
-
-# if __name__ == "__main__":
-#     robot_system = Movement()
-#     robot_system.run()
-#     rospy.spin()
-
-
 import rospy
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 import moveit_commander
 import math
-from sensor_msgs.msg import Image, LaserScan
 
 class Movement:
-    '''
-    Class for the movement of the robot
-    '''
     def __init__(self):
-        rospy.init_node('movement_controller')
-        rospy.loginfo("Initializing Movement Node")
+        rospy.init_node('robot_policy_executor')
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.scan_subscriber = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+        self.lidar_sub = rospy.Subscriber('/scan', LaserScan, self.lidar_callback)
         self.twist = Twist()
+        self.current_scan = None
+        self.safe_distance = 0.21
         self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
         self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
-        self.current_scan = None
         self.initialize_robot()
 
     def initialize_robot(self):
@@ -163,61 +25,8 @@ class Movement:
         self.move_group_gripper.stop()
         rospy.loginfo("Robot initialized to home position with gripper open.")
 
-    def pick_up_object(self):
-        rospy.loginfo("Picking up the object...")
-        self.move_group_arm.go([0, math.radians(18), math.radians(2), math.radians(0)], wait=True)
-        rospy.sleep(2)
-        self.move_group_gripper.go([-0.01, -0.01], wait=True)
-        rospy.sleep(2)
-        self.move_group_arm.go([0, -0.5, 0, 0], wait=True)
-        rospy.sleep(2)
-        self.move_group_arm.stop()
-        self.move_group_gripper.stop()
-        rospy.loginfo("Object picked up successfully.")
-
-    def spin_around(self):
-        rospy.loginfo("Spinning the robot...")
-        self.twist.angular.z = 0.5  # Spin speed
-        self.cmd_vel_pub.publish(self.twist)
-        rospy.sleep(5)  # Spin for 5 seconds
-        self.twist.angular.z = 0  # Stop the spin
-        self.cmd_vel_pub.publish(self.twist)
-        rospy.loginfo("Finished spinning.")
-
-    def move_towards_object(self, distance):
-        rospy.loginfo(f"Moving towards the object by {distance} meters...")
-        self.twist.linear.x = 0.2  # Set forward speed
-        self.cmd_vel_pub.publish(self.twist)
-        rospy.sleep(distance / 0.2)
-        self.twist.linear.x = 0  # Stop moving forward
-        self.cmd_vel_pub.publish(self.twist)
-        rospy.loginfo("Reached the object.")
-
-    def throw_away(self):
-        rospy.loginfo("Dumping the object into the bin at the back...")
-        
-        # Adjust the arm to a position behind the robot, suitable for dumping into the bin
-        dump_position = [0, -math.radians(30), -math.radians(10), math.radians(0)]  # Example: adjust angles as necessary
-        self.move_group_arm.go(dump_position, wait=True)
-        rospy.sleep(5)
-        
-        # Open the gripper to release the object into the bin
-        self.move_group_gripper.go([0.01, 0.01], wait=True)
-        rospy.sleep(5)
-        
-        # Return the arm to a default position
-        self.move_group_arm.go([0, 0, 0, 0], wait=True)
-        self.move_group_gripper.go([0.01, 0.01], wait=True)
-        self.move_group_arm.stop()
-        self.move_group_gripper.stop()
-        
-        rospy.loginfo("Object dumped successfully into the bin.")
-
-    def scan_callback(self, data):
-        '''
-        This callback processes LIDAR data to find the closest object
-        '''
-        self.current_scan = data
+    def lidar_callback(self, msg):
+        self.current_scan = msg
 
     def find_closest_object(self):
         if self.current_scan is None:
@@ -231,23 +40,57 @@ class Movement:
                 min_angle = i
         return min_distance, min_angle
 
+    def approach_closest_object(self, min_distance, min_angle):
+        angular_speed = 0.3
+        linear_speed = 0.2
+        target_angle = min_angle - 180 if min_angle > 180 else min_angle
+        rospy.loginfo(f"Approaching object at angle {min_angle} with distance {min_distance}")
+        while not rospy.is_shutdown():
+            if min_distance < self.safe_distance:
+                self.twist.linear.x = 0
+                self.twist.angular.z = 0
+                self.cmd_vel_pub.publish(self.twist)
+                rospy.loginfo("Object is within safe distance, stopping.")
+                break
+            self.twist.linear.x = linear_speed
+            self.twist.angular.z = -angular_speed if target_angle > 0 else angular_speed
+            self.cmd_vel_pub.publish(self.twist)
+            rospy.sleep(0.1)
+            min_distance, min_angle = self.find_closest_object()
+
+    def pick_up_object(self):
+        rospy.loginfo("Picking up the object...")
+        self.move_group_arm.go([0, math.radians(18), math.radians(2), math.radians(0)], wait=True)
+        rospy.sleep(2)
+        self.move_group_gripper.go([-0.01, -0.01], wait=True)
+        rospy.sleep(2)
+        self.move_group_arm.go([0, -0.5, 0, 0], wait=True)
+        rospy.sleep(2)
+        self.move_group_arm.stop()
+        self.move_group_gripper.stop()
+        rospy.loginfo("Object picked up successfully.")
+
+    def throw_away(self):
+        rospy.loginfo("Dumping the object into the bin at the back...")
+        dump_position = [0, -math.radians(30), -math.radians(10), math.radians(0)]
+        self.move_group_arm.go(dump_position, wait=True)
+        rospy.sleep(5)
+        self.move_group_gripper.go([0.01, 0.01], wait=True)
+        rospy.sleep(5)
+        self.move_group_arm.go([0, 0, 0, 0], wait=True)
+        self.move_group_gripper.go([0.01, 0.01], wait=True)
+        self.move_group_arm.stop()
+        self.move_group_gripper.stop()
+        rospy.loginfo("Object dumped successfully into the bin.")
+
     def run(self):
-        '''
-        Main logic:
-        - Spin around to scan the environment
-        - Move towards the closest object
-        - Pick up the object
-        - Throw away the object
-        '''
         rate = rospy.Rate(10)  # 10 Hz
         while not rospy.is_shutdown():
-            # Spin around to find objects
-            self.spin_around()
             closest_object = self.find_closest_object()
-            if closest_object is not None:
+            if closest_object:
                 min_distance, min_angle = closest_object
                 rospy.loginfo(f"Closest object at distance: {min_distance}, angle: {min_angle}")
-                self.move_towards_object(min_distance)
+                self.approach_closest_object(min_distance, min_angle)
                 self.pick_up_object()
                 self.throw_away()
             else:
@@ -255,6 +98,6 @@ class Movement:
             rate.sleep()
 
 if __name__ == "__main__":
-    robot_system = Movement()
-    robot_system.run()
+    executor = Movement()
+    executor.run()
     rospy.spin()
